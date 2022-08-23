8599cdfb1b67f2d698f45380585b623c0c3b633c
evgeniy <evgeniy_arefa@i.ua>
2022-08-23 23:05:31 +0300

    Added lesson 09

--- lesson_09/4.py
+++ lesson_09/4.py
@@ -12,7 +12,3 @@ in_list = (
 print(sorted(in_list))
 print(sorted(in_list, key=lambda x: x[1]))
 print(sorted(in_list, key=lambda x: (x[1], x[0])))
-
-sum_func = lambda num: (lambda x, y, z: x + y + z)(*num)
-
-print(sum_func((3, 5, 6)))
