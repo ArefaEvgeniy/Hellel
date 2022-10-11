93704970554f0276afaca26c57a02036d0d51c91
evgeniy <evgeniy_arefa@i.ua>
2022-10-11 21:33:08 +0300

    Added Lesson 05

--- lesson_05/2.py
+++ lesson_05/2.py
@@ -4,3 +4,14 @@
 # Если ввозраст больше 12 и меньше 18 - вывести CocaCola
 # Иначе - вывести Beer
 
+
+age = input("введите ваш возраст ")
+
+if not age.isdigit() or int(age) < 0:
+    print("Wrong input")
+elif int(age) <= 12:
+    print('Orange')
+elif int(age) < 18:
+    print('CocaCola')
+else:
+    print('Beer')
