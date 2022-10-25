3e71d5673e2629e5adc5c1bacf7c32ad4a943f13
evgeniy <evgeniy_arefa@i.ua>
2022-10-25 21:24:34 +0300

    Added Lesson 08

--- lesson_08/2.py
+++ lesson_08/2.py
@@ -3,3 +3,12 @@
 
 scores = [66, 90, 68, 59, 76, 60, 88, 74, 81, 65, 75, 99, 12]
 
+
+def is_A_student(x):
+    return x > 75
+
+
+over_75 = list(filter(is_A_student, scores))
+over_75_2 = list(filter(lambda x: x > 75, scores))
+print(over_75)
+print(over_75_2)
--- lesson_08/3.py
+++ lesson_08/3.py
@@ -2,3 +2,16 @@
 # Функция sum() возвращает сумму всех элементов итерируемого объекта,
 # переданных ей.
 
+
+from functools import reduce
+
+
+def custom_sum(first, second):
+    return first + second ** 2
+
+
+result = reduce(custom_sum, {1, 2, 3, 4, 5, 6, 7})
+print(result)
+
+result_2 = reduce(custom_sum, (1, 2, 3, 4, 5, 6, 7))
+print(result_2)
