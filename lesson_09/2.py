726bc398e8d68bd7b3bbc132d7a022f1ec8f8f0f
evgeniy <evgeniy_arefa@i.ua>
2022-10-28 21:36:34 +0300

    Added Lesson 09

--- lesson_09/2.py
+++ lesson_09/2.py
@@ -9,3 +9,34 @@ INPUT_LIST = [
     ['one', 'two', '55', {1, 4, 'big', True}, ['milk', 0, 'bred']],
     'End'
 ]
+
+
+def find_word(word, INPUT_LIST):
+    result = False
+
+    for item in INPUT_LIST:
+        if isinstance(item, (str, int)) and item == word:
+            result = True
+            break
+        elif isinstance(item, (list, tuple)):
+            result = find_word(word, item)
+            if result:
+                break
+        elif isinstance(item, set):
+            if word in item:
+                result = True
+                break
+
+    return result
+
+
+def main():
+    input_word = input("Введите слово: ")
+
+    if find_word(input_word, INPUT_LIST):
+        print("Слово найдено")
+    else:
+        print("Слово не найдено")
+
+
+main()
