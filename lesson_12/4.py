1cbef768add59e2293006ea65b24a01a4d6c4b2d
evgeniy <evgeniy_arefa@i.ua>
2022-11-08 21:21:39 +0200

    Added lesson 12

--- lesson_12/4.py
+++ lesson_12/4.py
@@ -1,31 +1,26 @@
-class Dog:
-    number_of_foot = 4
-    viviparous = True
-    tail = True
-    name = None
+from abc import ABC, abstractmethod
 
+
+class Animal(ABC):
     def __init__(self, name, number_of_foot=4, tail=True):
         self.name = name
         self.number_of_foot = number_of_foot
         self.tail = tail
 
+    @abstractmethod
     def say(self):
-        print('Woof, woof')
+        ...
 
     def go(self):
         for item in range(1, self.number_of_foot + 1):
             print(f'step {item}')
 
 
-dog_1 = Dog('Bob', 3)
+class Dog(Animal):
+    def say(self):
+        print('Woof, woof')
+
+
+dog_1 = Dog('Bob')
+dog_1.go()
 dog_1.say()
-dog_1.go()
-print(dog_1.number_of_foot)
-
-dog_1.go()
-
-print('-' * 50)
-
-dog_2 = Dog('Rem')
-print(dog_2.number_of_foot)
-dog_2.go()
