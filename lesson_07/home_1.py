3be55de2439cf1b75564dd61c9af413271ef2a4a
evgeniy <evgeniy_arefa@i.ua>
2022-11-08 21:24:46 +0200

    Added solutions of homeworks for lessons 6 and 7

--- lesson_07/home_1.py
+++ lesson_07/home_1.py
@@ -4,3 +4,28 @@
 # а значение - сколько раз оно встречается). Для проверки нахождения элемента
 # в словаре использовать метод get(), либо оператор in.
 
+from random import randint
+
+# Генерация случайного списка
+numbers = []
+for _ in range(50):
+  numbers.append(randint(1, 10))
+print("Первоначальный список: ", numbers)
+
+
+def entries_count(sequence: list) -> dict:
+  """Функция подсчета кол-ва чисел в списке"""
+  result = {}
+  for item in sequence:
+    if item in result:
+      result[item] += 1
+    else:
+      result[item] = 1
+
+  return result
+
+
+result = entries_count(numbers)
+for item in result:
+  print(f"Число {item} встречается в первоначальном списке "
+        f"{result[item]} раз")
